89b3d39564858e1c45d3f54fbb3b5a9171983474
dev0ps221 <seybaxscorpion@gmail.com>
2022-07-01 12:54:20 +0000

    commands

--- core/classes/Command.py
+++ core/classes/Command.py
@@ -2,9 +2,11 @@
 class Command:
 
 
-
+    def run(self,args):
+        self.action(args)
 
     def __init__(self,name,func):
+        print('command created')
         self.name = name 
         self.action = func
 
