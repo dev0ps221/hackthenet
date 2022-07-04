8b4d1866f4db3bf114ef785e5fd276cd9e66370c
dev0ps221 <seybaxscorpion@gmail.com>
2022-07-04 02:12:38 +0000

    module

--- core/classes/Module.py
+++ core/classes/Module.py
@@ -1,6 +1,7 @@
 class Module:
 
 
+    actual_target = (-1,None)
     actions = {}
 
     def runAction(self,func):
@@ -16,6 +17,22 @@ class Module:
 
     def add_target(self,target):
         self.config['targets'].append(target)
+    
+    def remove_target(self,target):
+        self.config['targets'] = []
+        for tgt in self._targets:
+            self.add_target(tgt) if tgt != target
+        return self._targets()
+
+    def next_target(self):
+        idx,target = self.actual_target
+        targets_size = len(self._targets)
+        idx+=1
+        if idx < targets_size :
+            self.actual_target = idx,self._targets()[idx]
+        else  
+            self.actual_target = -1,None
+        return self.actual_target[1]
 
     def __init__(self,name,config={targets:[]}):
         self.name = name
