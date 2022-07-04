6e9891c72af5022492272701fa1929cb9562c62c
dev0ps221 <seybaxscorpion@gmail.com>
2022-07-04 02:04:45 +0000

    module

--- core/classes/Module.py
+++ core/classes/Module.py
@@ -11,6 +11,13 @@ class Module:
     def register_action(self,name,func):
         self.actions[name] = self.runAction(func)
 
-    def __init__(self,name):
-        self.name = name
+    def _targets(self):
+        return self.config['targets']
 
+    def add_target(self,target):
+        self.config['targets'].append(target)
+
+    def __init__(self,name,config={targets:[]}):
+        self.name = name
+        self.config = config
+        
