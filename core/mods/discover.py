11a849de47dba2c7a6f8f14708c5ceef99d2ad2e
dev0ps221 <seybaxscorpion@gmail.com>
2022-07-04 10:12:52 +0000

    discover

--- core/mods/discover.py
+++ core/mods/discover.py
@@ -1,34 +1,15 @@
 from os import getcwd
-__import__(f'..',globals(),locals())
-__import__(f'..',globals(),locals())
-__import__(f'..',globals(),locals())
+from sys import path
+insert = path.insert
+  
+insert(0, getcwd()+'/../classes')
+from Module import *
 
 
 
-class Discover(Module):
-
-
-    def initialize_actions(self):
-        self.register_action(
-            'add_commande',lambda self,name,action:self.shell.add_commande(name,action)
-        )
-
-    def set_shell_commands(self):
-        def show(command,self,arg):
-            print(command,self,arg)
-            if arg:
-                print('asked to show ',arg)
-        self.runAction(
-            'add_commande','show',show
-        )
-
-    def initialize_shell(self):
-        self.shell = Shell({},self)
-
+class Discover (Module):
     def __init__(self,name):
         Module.__init__(self,name)
-        self.initialize_shell()
-        self.initialize_actions()
         
-d = Discover()
-Discover.shell.getCmd('show').run()
+d = Discover('discover')
+d.shell.getCmd('show').run()
