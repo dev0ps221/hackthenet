c6b70842e6a5106683409b18d439f2d29b9e55a8
dev0ps221 <seybaxscorpion@gmail.com>
2022-07-08 13:43:05 +0000

    config | Core objects

--- core/classes/Core.py
+++ core/classes/Core.py
@@ -3,6 +3,20 @@ class IpAddress:
     hostpart = None
     netpart = None
     def has(self,name=None):
+        return (hasattr(self,name) and self[name] is not None) if name else name
+    
+    def has_address(self):
+        return self.has('address')
+
+    def set_address(self,address=None):
+        self.address = address
+
+    def get_address(self):
+        return self.address if self.has_address() else self.has_address()
+
+class MacAddress:
+    address = None
+    def has(self,name=None):
         return hasattr(self,name) and self[name] is not None if name else name
     
     def has_address(self):
@@ -14,13 +28,48 @@ class IpAddress:
     def get_address(self):
         return self.address if self.has_address() else self.has_address()
 
+class Port:
+    number = None
+    ip = None
+    def has(self,name=None):
+        return hasattr(self,name) and self[name] is not None if name else name
 
+    def set_number(port):
+        self.number = port
+
+    def has_number(self):
+        return self.has('number')
+
+
+    def get_number(self):
+        return self.number if self.has_number() else self.has_number()
+
+    def set_ip(port):
+        self.ip = port
+
+    def has_ip(self):
+        return self.has('ip')
+
+
+    def get_ip(self):
+        return self.ip if self.has_ip() else self.has_ip()
+
+    def __init__(self,port=None,ip=None):
+        if port:
+            set_number(port)
+        if ip:
+            set_ip(ip)
 
 class Host:
     ip = IpAddress()
+    mac = MacAddress()
+    network = Network()
     ports = []
     targeted_ports = []
     known_ports = []
+
+    def has(self,name=None):
+        return hasattr(self,name) and self[name] is not None if name else name
     
     def get_targeted_port(self,port):
         matches = self.targeted_ports.filter(lambda a:a.number==port)
@@ -32,4 +81,7 @@ class Host:
 
 
 class Network:
+    hosts = []
+    targeted_hosts = []
+
 
